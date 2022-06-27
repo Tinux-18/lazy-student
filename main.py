2968c9412a8651ae7f86bfb6f3b87af1f45d36b8
Constantin Rigu <rigucatalin@gmail.com>
2022-06-27 14:12:02 +0200

    Ready for frontend

--- main.py
+++ main.py
@@ -19,66 +19,67 @@ db = client.time_tracking
 target_student_name = "Samuel Cochran"
 target_course_id = ObjectId("62b707d4e700f67ac1e2eff7")
 
-#Find out minutes logged
+# Aggregate timelog
 
-minutes = db.timelog.aggregate(
-    [
-    {'$match': {'name': target_student_name, 'course_id': target_course_id}},
-       {
-          "$group":
-             {
-                 "_id": {
-             "name": "$name",
-             "course_id": "$course_id"
-            },
-                 "averageTime":
-                    {
-                       "$avg":
-                          {
-                             "$dateDiff":
-                                {
-                                    "startDate": "$login",
-                                    "endDate": "$logout",
-                                    "unit": "minute"
-                                }
-                           }
-                    }
-             }
-       },
-        {
-            "$lookup":
-                {
-                    "from": "courses",
-                    "localField": "_id.course_id",
-                    "foreignField": "_id",
-                    "as": "course"
-                }
-        },
-{
-    "$unwind": "$course"
-},
-{
-        "$project": {
-          "_id": 1,
-          "name": 1,
-          "averageHours": {
-                      "$divide": [ "$averageTime", 60 ]
-                   },
-          "course.title": 1,
-          "course.duration": 1,
-          "progress": {"$divide": [
-                        {"$multiply": [
-                            {"$divide": [
-                                "$averageTime",
-                                60]
-                            },
-                            100]
-                        },
-                        "$course.duration"]}
-        }
-      }
-    ]
-)
+# minutes = db.timelog.aggregate(
+#     [
+#     {'$match': {'name': target_student_name, 'course_id': target_course_id}},
+#        {
+#           "$group":
+#              {
+#                  "_id": {
+#              "name": "$name",
+#              "course_id": "$course_id"
+#             },
+#                  "averageTime":
+#                     {
+#                        "$avg":
+#                           {
+#                              "$dateDiff":
+#                                 {
+#                                     "startDate": "$login",
+#                                     "endDate": "$logout",
+#                                     "unit": "minute"
+#                                 }
+#                            }
+#                     }
+#              }
+#        },
+#         {
+#             "$lookup":
+#                 {
+#                     "from": "courses",
+#                     "localField": "_id.course_id",
+#                     "foreignField": "_id",
+#                     "as": "course"
+#                 }
+#         },
+# {
+#     "$unwind": "$course"
+# },
+# {
+#         "$project": {
+#           "_id": 1,
+#           "name": 1,
+#           "averageHours": {
+#                       "$divide": [ "$averageTime", 60 ]
+#                    },
+#           "course.title": 1,
+#           "course.duration": 1,
+#           "progress": {"$divide": [
+#                         {"$multiply": [
+#                             {"$divide": [
+#                                 "$averageTime",
+#                                 60]
+#                             },
+#                             100]
+#                         },
+#                         "$course.duration"]}
+#         }
+#       }
+#     ]
+# )
+#
+# for x in minutes:
+#     print(x)
 
-for x in minutes:
-    print(x)
