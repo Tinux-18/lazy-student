6c4ee3287e5552a79fa7216b4ce5279822574204
Constantin Rigu <rigucatalin@gmail.com>
2022-06-27 18:42:33 +0200

    Fix minor indentation and path issues

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ from query_mongo import get_progress
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def get_lists():
     print("get me")
     # minutes = get_progress("Samuel Cochran", "62b707d4e700f67ac1e2eff7")._CommandCursor__data[0]
+
 
 @app.route('/', methods=['POST'])
 def get_progress():
